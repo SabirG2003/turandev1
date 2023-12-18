38abace1b8bf3e130fd75aa0d01f26c26c57b809
Sabir Gunashev <sabir@Sabirs-MacBook-Pro.local>
2023-12-18 09:23:33 -0600

    shell scipt file on how to download ansible

---
+++ ansible_install.sh
@@ -0,0 +1,17 @@
+#to download ansible we first have to install pip
+
+#run following command to install pip 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+
+
+#after you run both those commads you can check if it succefully downloaded by running
+python3 --version
+#if it gives a version that means it succesfully downloaded
+
+#now that we pip we can download ansible
+#run following command to download ansible
+pip install ansible
+
+#wait for it download and install
+#done
